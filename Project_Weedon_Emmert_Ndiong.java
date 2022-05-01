5e2d100c9024455b3688bb20efff8cc79ace57b2
mndiong <mndion2@students.towson.edu>
2022-05-01 14:03:40 -0400

    Update Project_Weedon_Emmert_Ndiong.java

--- Project_Weedon_Emmert_Ndiong.java
+++ Project_Weedon_Emmert_Ndiong.java
@@ -9,9 +9,9 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class GetProcessList {
-    
+
     private String GetProcessListData() {
-        
+
         Process p;
         Runtime runTime;
         String process = null;
@@ -48,7 +48,7 @@ public class GetProcessList {
         }
         return process;
     }
- 
+
     private void showProcessData() {
         try {
 
@@ -57,19 +57,19 @@ public class GetProcessList {
  
     //Create Streams for write processes
     //Given the filepath which you need.Its store the file at where your java file.
-    
+
     OutputStreamWriter outputStreamWriter =
             new OutputStreamWriter(new FileOutputStream("ProcessList.txt"));
     BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
- 
+
     //Tokenize the output for write the processes
     StringTokenizer st = new StringTokenizer(proc, "&");
- 
+
     while (st.hasMoreTokens()) {
         bufferedWriter.write(st.nextToken());  //Write the data in file
         bufferedWriter.newLine();               //Allocate new line for next line
     }
- 
+
     //Close the outputStreams
     bufferedWriter.close();
     outputStreamWriter.close();
@@ -77,9 +77,10 @@ public class GetProcessList {
             ioe.printStackTrace();
         }
     }
- 
+
     public static void main(String[] args) {
         GetProcessList gpl = new GetProcessList();
         gpl.showProcessData();
     }
 }
+
