c3fa81030945b0e01cba0f3e6ea1ed681165e211
gweedo1 <95831619+gweedo1@users.noreply.github.com>
2022-05-09 15:50:41 -0400

    Open application

---
+++ openNotepad.java
@@ -0,0 +1,17 @@
+//Java program to run an application.
+
+import java.util.*;
+
+class openNotepad {
+  public static void main(String[] args) {
+    Runtime app = Runtime.getRuntime();
+    try {
+      //open notepad
+      app.exec("notepad");
+      //open calculator
+      app.exec("calc");
+    } catch (Exception Ex) {
+      System.out.println("Error: " + Ex.toString());
+    }
+  }
+}
